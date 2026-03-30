4a8dcbaa68b279b079e61aaea0ad8a96b1e70ae5
Tuong <ttruong2621@sdsu.edu>
2026-03-30 15:49:15 -0700

    Finishing connectingbackend and frontend locally

--- frontend/script.js
+++ frontend/script.js
@@ -3,16 +3,16 @@
 const BACKEND_URL = "http://127.0.0.1:8787";
 
 async function getLatestEvent(){
-    try{const response = await fetch('${BACKEND_URL}/latest-event');
+    try{const response = await fetch(`${BACKEND_URL}/latest-event`);
     const data = await response.json();
 
     console.log("Received:", data);
     
     //if there's an event, update the UI
     if(data && data.event) {
-        const el = document.getElemementById("statusText");
+        const el = document.getElementById("statusText");
 
-        el.textContent = '${data.soundName} detected';
+        el.textContent = `${data.soundName} detected`;
         el.style.color = "red";
     }
     }
