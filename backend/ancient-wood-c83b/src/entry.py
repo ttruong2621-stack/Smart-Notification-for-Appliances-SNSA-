fb2294c862091df9c30e86347f593ae610f780f2
Tuong <ttruong2621@sdsu.edu>
2026-03-30 15:08:13 -0700

    adding main.py version of fastapi

--- backend/ancient-wood-c83b/src/entry.py
+++ backend/ancient-wood-c83b/src/entry.py
@@ -1,7 +1,31 @@
 from workers import Response, WorkerEntrypoint
-from submodule import get_hello_message
+import json
+
+# fake database to storage the latest event send by SNSA
+latest_event = {}
 
 
 class Default(WorkerEntrypoint):
     async def fetch(self, request):
-        return Response(get_hello_message())
+        global latest_event
+
+        # extract the request into url, method and path
+        url = request.url
+        method = request.method
+        path = url.split(".workers.dev")[-1]
+
+        if path == "/event" and method == "POST":
+            # expect the request body to be a json object and store it in the latest_event variable
+            body = await request.json()
+            latest_event = body
+
+            # here the response confirming correct reception of the event is sent back to the client
+            return Response(
+                json.dumps({"success": True}),
+                headers={"Content-Type": "application/json"},
+            )
+        if path == "/latest-event" and method == "GET":
+            return Response(
+                json.dumps(latest_event), headers={"Content-Type": "application/json"}
+            )
+        return Response("Not Found", status=404)
